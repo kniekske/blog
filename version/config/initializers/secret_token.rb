09f9cc5e657d88514f5bea23239104b07bacf655
Kniekske <moniekverlaek@gmail.com>
2014-11-05 11:57:13 +0800

    twitter bootstrap

---
+++ version/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Version::Application.config.secret_token = '040463aa1d2d74339d3c8971320f49976b518abbda47b70c628499c92689d7e126422a8a6c6e1b87912406cfbf463a0d6f168187a028d0370eeea9660790cb03'
